b870c8436087b52594ec6d77e7ad69a81f11485a
Pascal <pascalokereke18@gmail.com>
2024-03-12 12:29:33 +0100

    adjustment

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-
+import backgroundImage from '../assets/baller.jpg'
 
 const LandingPage = () => {
     const [showLogin, setShowLogin ] = useState(false);
@@ -31,12 +31,12 @@ const LandingPage = () => {
 
 
    return (
-    <div className='flex justify-center items-center min-h-screen min-w-screen bg-cover bg-center' style={{backgroundImage: `url('../src/assets/baller.jpg')`}}>
+    <div className='flex justify-center items-center min-h-screen min-w-screen bg-cover bg-center' style={{backgroundImage: `url(${backgroundImage})`}}>
         <div className='mx-auto min-w-0'>
         <h1 className='text-center text-7xl relative bottom-40 text-white font-whisper'>WorthSport</h1>
-        <div className='flex flex-col border border-pink-100 rounded-2xl p-36 py-8 bg-black bg-opacity-75'>
-        <button onClick={handleLoginClick} className='m-2 border rounded-t-full px-10 text-white hover:bg-blue-800 font-whisper text-3xl'>Login</button>
-        <button onClick={handleSignupClick} className='border rounded-t-full px-10 text-white hover:bg-blue-800 font-whisper text-3xl'>Sign Up</button>
+        <div className='flex flex-col border border-pink-100 rounded-2xl p-36 py-8 xs:rounded-md md:rounded-lg bg-black xs:bg-opacity md:bg-opacity-75 bg-opacity-75'>
+        <button onClick={handleLoginClick} className='m-2 border rounded-full relative px-6 py-3 xs:border xs:rounded-md xs:relative md:border md:rounded-lg md:relative text-white hover:bg-blue-800 text-xl'>Login</button>
+        <button onClick={handleSignupClick} className='border rounded-full relative px-6 py-3 xs:border xs:relative xs:rounded-md md:border md:rounded-lg md:relative text-white hover:bg-blue-800 text-xl'>Sign Up</button>
     </div>
     </div>
     </div>
