096d34b30ee7d6ec2fa40990542c662721d5a032
Pascal <pascalokereke18@gmail.com>
2024-04-19 17:28:29 +0100

    database config

--- Database/post.sql
+++ Database/post.sql
@@ -2,6 +2,7 @@ CREATE TABLE users (
     id SERIAL PRIMARY KEY,
     first_name VARCHAR(50) NOT NULL,
     last_name VARCHAR(50) NOT NULL,
+    gender ENUM('M', 'F'),
     email VARCHAR(100) UNIQUE NOT NULL,
     password VARCHAR(100) NOT NULL,
     is_verified BOOLEAN DEFAULT FALSE,
